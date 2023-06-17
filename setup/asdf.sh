96f2c88fd658876f6cbcbc407ab592a8698493cc
David Rueck <drueck@gmail.com>
2023-06-17 16:12:17 -0700

    Don't use asdf oh-my-zsh plugin, just activate normally

--- setup/asdf.sh
+++ setup/asdf.sh
@@ -3,6 +3,10 @@
 if [[ ! -d ~/.asdf ]]; then
   echo "Installing asdf."
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
+
+  echo "Adding code to activate asdf to your .zshrc.local..."
+  echo '. "$HOME/.asdf/asdf.sh"' >> ~/.zshrc.local
 else
   echo "asdf is installed."
 fi
+
