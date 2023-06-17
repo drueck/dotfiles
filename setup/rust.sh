4861749db5f4d1b595cd07c27c158a2f0b443178
David Rueck <drueck@gmail.com>
2023-06-17 16:12:54 -0700

    Don't add ~/.cargo/bin to the $PATH here
    The rust installer will add code to `.zshenv` to handle this properly

--- setup/rust.sh
+++ setup/rust.sh
@@ -4,9 +4,6 @@ if ! command -v rustup &> /dev/null; then
   echo "Installing rust..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-  echo "Adding ~/.cargo/bin to the PATH..."
-  echo "export PATH=\$PATH:$HOME/.cargo/bin" >> ~/.zshrc.local
-
   echo "Please restart your shell to start using rust."
 else
   echo "Rust is installed."
