c6b941495536a5c7585f57a9f9b7df7b3fcd7043
David Rueck <drueck@gmail.com>
2023-06-17 16:37:40 -0700

    Add completions config as well for asdf

--- setup/asdf.sh
+++ setup/asdf.sh
@@ -4,9 +4,15 @@ if [[ ! -d ~/.asdf ]]; then
   echo "Installing asdf."
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 
-  echo "Adding code to activate asdf to your .zshrc.local..."
-  echo '. "$HOME/.asdf/asdf.sh"' >> ~/.zshrc.local
+  echo "Add asdf config to .zshrc.local..."
+  cat <<'EOF' >> ~/.zshrc.local
+# actiate asdf
+. "$HOME/.asdf/asdf.sh"
+
+# configure asdf completions
+fpath=(${ASDF_DIR}/completions $fpath)
+autoload -Uz compinit && compinit
+EOF
 else
   echo "asdf is installed."
 fi
-
