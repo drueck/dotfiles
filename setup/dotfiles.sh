dd4211a7a48ffffc47623a1124b43a5534ebc49a
David Rueck <drueck@gmail.com>
2023-06-17 10:43:18 -0700

    Clean up zshrc, aliases, and zshrc.local; fix dotfiles bugs

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -5,8 +5,20 @@ FILES_DIR=~/projects/dotfiles/files
 echo "Symlinking dotfiles..."
 
 ln -sf $FILES_DIR/zshrc ~/.zshrc
-ln -sf $FILES_DIR/zshrc.local ~/.zshrc.local
-ln -sf $FILES_DIR/aliases ~/.aliases
 ln -sf $FILES_DIR/tmux.conf ~/.tmux.conf
 ln -sf $FILES_DIR/gitconfig ~/.gitconfig
-ln -sf $FILES_DIR/bin ~/.bin
+
+if [[ ! -d ~/.bin ]]; then
+  echo "Linking .bin directory..."
+  ln -sf $FILES_DIR/bin ~/.bin
+fi
+
+if [[ ! -f ~/.zshrc.local ]]; then
+  echo "Copying .zshrc.local..."
+  cp $FILES_DIR/zshrc.local ~/.zshrc.local
+fi
+
+if [[ ! -f ~/.aliases ]]; then
+  echo "Copying .aliases..."
+  cp $FILES_DIR/aliases ~/.aliases
+fi
