ad248d1e4c967b1a73c78c3c811ef476c7c59e5a
David Rueck <drueck@gmail.com>
2023-07-12 15:13:15 -0700

    Add in eslint again (and autoformat the lua, apparently)

--- files/config/nvim/settings/plugins/mason.lua
+++ files/config/nvim/settings/plugins/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "rust_analyzer", "elixirls", "tsserver", "pyright", "ruby_ls" }
-}
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "rust_analyzer", "elixirls", "tsserver", "eslint", "pyright", "ruby_ls" },
+})
