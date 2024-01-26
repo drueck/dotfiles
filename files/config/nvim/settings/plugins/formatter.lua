4ace4f10718205675a8e1b109f72282ddd820d86
David Rueck <drueck@gmail.com>
2024-01-26 15:21:14 -0800

    Reformat file and add terraform formatter, everything enabled

--- files/config/nvim/settings/plugins/formatter.lua
+++ files/config/nvim/settings/plugins/formatter.lua
@@ -1,35 +1,38 @@
 require("formatter").setup({
-	logging = true,
-	log_level = vim.log.levels.WARN,
-	filetype = {
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		javascript = {
-			require("formatter.filetypes.javascript").prettier,
-		},
-		javascriptreact = {
-			require("formatter.filetypes.javascriptreact").prettier,
-		},
-		typescript = {
-			require("formatter.filetypes.typescript").prettier,
-		},
-		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettier,
-		},
-		json = {
-			require("formatter.filetypes.json").prettier,
-		},
-		python = {
-			require("formatter.filetypes.python").black,
-		},
+  logging = true,
+  log_level = vim.log.levels.WARN,
+  filetype = {
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+    javascript = {
+      require("formatter.filetypes.javascript").prettier,
+    },
+    javascriptreact = {
+      require("formatter.filetypes.javascriptreact").prettier,
+    },
+    typescript = {
+      require("formatter.filetypes.typescript").prettier,
+    },
+    typescriptreact = {
+      require("formatter.filetypes.typescriptreact").prettier,
+    },
+    json = {
+      require("formatter.filetypes.json").prettier,
+    },
+    python = {
+      require("formatter.filetypes.python").black,
+    },
     rust = {
       require("formatter.filetypes.rust").rustfmt,
     },
     elixir = {
-      require("formatter.filetypes.elixir").mixformat
-    }
-	},
+      require("formatter.filetypes.elixir").mixformat,
+    },
+    terraform = {
+      require("formatter.filetypes.terraform").terraformfmt,
+    },
+  },
 })
 
 local autoformat_types = {
@@ -38,18 +41,19 @@ local autoformat_types = {
   javascriptreact = true,
   json = true,
   lua = true,
-  python = false,
+  python = true,
   rust = true,
+  terraform = true,
   typescript = true,
   typescriptreact = true,
 }
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  desc = 'Autoformat certain types of files on save',
-  group = vim.api.nvim_create_augroup('autosave', { clear = true }),
-  callback = function (opts)
+vim.api.nvim_create_autocmd("BufWritePost", {
+  desc = "Autoformat certain types of files on save",
+  group = vim.api.nvim_create_augroup("autosave", { clear = true }),
+  callback = function(opts)
     if autoformat_types[vim.bo[opts.buf].filetype] then
-      vim.cmd 'FormatWrite'
+      vim.cmd("FormatWrite")
     end
   end,
 })
